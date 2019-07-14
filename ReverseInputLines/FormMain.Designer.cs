c5b2106ccc5b6ea2fd9e3e85d118b505f2b700cc
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-14 14:53:43 +0300

    Task_Done

---
+++ ReverseInputLines/FormMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ReverseInputLines
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoadFile = new System.Windows.Forms.Button();
+            this.BtnReverse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnLoadFile
+            // 
+            this.BtnLoadFile.Location = new System.Drawing.Point(35, 12);
+            this.BtnLoadFile.Name = "BtnLoadFile";
+            this.BtnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoadFile.TabIndex = 0;
+            this.BtnLoadFile.Text = "Load";
+            this.BtnLoadFile.UseVisualStyleBackColor = true;
+            this.BtnLoadFile.Click += new System.EventHandler(this.BtnLoadFile_Click);
+            // 
+            // BtnReverse
+            // 
+            this.BtnReverse.Location = new System.Drawing.Point(35, 53);
+            this.BtnReverse.Name = "BtnReverse";
+            this.BtnReverse.Size = new System.Drawing.Size(75, 23);
+            this.BtnReverse.TabIndex = 1;
+            this.BtnReverse.Text = "Reverse";
+            this.BtnReverse.UseVisualStyleBackColor = true;
+            this.BtnReverse.Click += new System.EventHandler(this.BtnReverse_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(138, 88);
+            this.Controls.Add(this.BtnReverse);
+            this.Controls.Add(this.BtnLoadFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormMain";
+            this.Text = "Reverse input lines";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoadFile;
+        private System.Windows.Forms.Button BtnReverse;
+    }
+}
+
---
+++ ReverseInputLines/FormMain.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReverseInputLines
+{
+    public partial class FormMain : Form
+    {
+        private OpenFileDialog openFile = new OpenFileDialog();
+        private List<string> listTextLines = new List<string>();
+        private Stack<string> stackTextLines = new Stack<string>();
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            CenterToScreen();
+        }
+
+        private void BtnLoadFile_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                SaveLines2Collections(openFile.FileName);
+            }
+        }
+
+        private void SaveLines2Collections(string path)
+        {
+            listTextLines = new List<string>();
+            stackTextLines = new Stack<string>();
+
+            string[] allLines = File.ReadAllLines(path);
+
+            if (path.Contains(".txt"))
+            {
+                listTextLines = allLines.ToList();
+
+                for (int i = 0; i < allLines.Length; i++)
+                {
+                    stackTextLines.Push(allLines.ElementAtOrDefault(i));
+                }
+            }
+        }
+
+        private void BtnReverse_Click(object sender, EventArgs e)
+        {
+            string listLinesDir = Environment.CurrentDirectory + @"\listLines.txt";
+            string stackLinesDir = Environment.CurrentDirectory + @"\stackLines.txt";
+
+            SaveListLines2Text(listTextLines, listLinesDir);
+            SaveStackLines2Text(stackTextLines, stackLinesDir);
+
+            Process.Start("notepad.exe", listLinesDir);
+            Process.Start("notepad.exe", stackLinesDir);
+        }
+
+        private void SaveListLines2Text(List<string> listTextLines, string saveDir)
+        {
+            StringBuilder lineBuilder = new StringBuilder();
+            string saveLines = string.Empty;
+
+            listTextLines.Reverse();
+
+            if (listTextLines.Count <= 8)
+            {
+                for (int i = 0; i < listTextLines.Count; i++)
+                {
+                    saveLines += (listTextLines.ElementAtOrDefault(i) + Environment.NewLine);
+                }
+                File.WriteAllText(saveDir, saveLines);
+            }
+            else
+            {
+                for (int i = 0; i < listTextLines.Count; i++)
+                {
+                    lineBuilder.AppendLine(listTextLines.ElementAtOrDefault(i));
+                }
+                File.WriteAllText(saveDir, lineBuilder.ToString());
+            }
+        }
+
+        private void SaveStackLines2Text(Stack<string> stackTextLines, string saveDir)
+        {
+            StringBuilder lineBuilder = new StringBuilder();
+            string saveLines = string.Empty;
+
+            if (listTextLines.Count <= 8)
+            {
+                foreach (var line in stackTextLines)
+                {
+                    saveLines += (line + Environment.NewLine);
+                }
+                File.WriteAllText(saveDir, saveLines);
+            }
+            else
+            {
+                foreach (var line in stackTextLines)
+                {
+                    lineBuilder.AppendLine(line);
+                }
+                File.WriteAllText(saveDir, lineBuilder.ToString());
+            }
+        }
+    }
+}
